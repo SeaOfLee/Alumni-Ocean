b13c9dfea434790f43f64f024eb541a01031aed1
laurenpawell <lauren.pawell@gmail.com>
2015-03-15 12:38:49 -0700

    Stub authorization on jobs and events controller

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe ActivitiesController do
   before(:each) do
+    controller.stub(:authorize)
     @event1 = Activity.create(event_name:'Test Event', event_description:'This is the description.', event_location:'Los Angeles', event_site:'GA', event_date:'February 2, 2015', event_time:'7:00PM PST')
     @event2 = Activity.create(event_name:'Tests Event', event_description:'This is description.', event_location:'Santa Monica', event_site:'General Assembly', event_date:'February 22, 2015', event_time:'9:00PM PST')
   end
